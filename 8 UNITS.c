a5109b9f34d6d34b90836246eba013dc1af4e27d
sadamabdi169-jpg <sadamabdi169@gmail.com>
2025-09-25 16:58:58 +0300

    Add files via upload

---
+++ 8 UNITS.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/* Name:SADAM ABDI MUHUMED 
+REG:CT101/G/29275/25
+Description assignment of week 1
+*/
+
+
+int main (){
+//  8 units
+printf ("ICT fundamentals \n");
+printf ("computer architecture \n");
+ printf ("commutation skills \n");
+ printf ("analogue electronics\n");
+ printf (" physics \n)");
+ printf("mathematics for science \n ");
+ printf ("discrete mathematics \n");
+ printf ("programming \n");
+ return 0;
+ }  
---
+++ assignment1.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+/* Name:SADAM ABDI MUHUMED 
+REG:CT101/G/29275/25
+Description assignment of week 1
+*/
+int main()
+{
+int height;
+int age;
+int phonenumber;
+int bankbalance;
+printf ("1. Enter your height ");
+scanf ("%d",& height);
+  printf ("your height is %d.\n",height);
+  printf ("2.Enter your age");
+  scanf ("%d",& age); 
+  printf (" your age is %d.\n",age);
+  printf ("3.Enter your phonenumber");
+  scanf("%d",& phonenumber);
+  printf ("your phonenumber is %d.\n",phonenumber);
+  printf("4.bankbalance");
+  scanf ("%d",&bankbalance);
+  printf ("your bankbalance is%d.\n",bankbalance);
+ 
+ 
+  return 0; 
+  } 
+  
+    
